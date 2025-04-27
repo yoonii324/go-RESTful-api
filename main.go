7ba2dbcf53637cf52391b964b43a07327c342e72
yoonii324 <jyoonii324@gmail.com>
2025-04-27 00:21:17 -0400

    refactored main.go: using gin gonic instead of gorilla mux

--- main.go
+++ main.go
@@ -1,40 +1,60 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 	"sort"
 	"strconv"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	//"github.com/gorilla/mux"
 )
 
+// type Student struct {
+// 	Id    int
+// 	Name  string
+// 	Age   int
+// 	Score int
+// }
+
 type Student struct {
-	Id    int
-	Name  string
-	Age   int
-	Score int
+	Id    int    `json:"id,omitempty"`
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Score int    `json:"score,omitempty"`
 }
 
 var students map[int]Student
 var lastId int
 
-func MakeWebHandler() http.Handler {
-	mux := mux.NewRouter()
-	mux.HandleFunc("/students", GetStudentListHandler).Methods("GET")
-	mux.HandleFunc("/students/{id:[0-9]+}", GetStudentHandler).Methods("GET")
-	mux.HandleFunc("/students", PostStudentHandler).Methods("GET")
-	mux.HandleFunc("/students/{id:[0-9]+}", DeleteStudentHandler).Methods("DELETE")
+// func MakeWebHandler() http.Handler {
+// 	mux := mux.NewRouter()
+// 	mux.HandleFunc("/students", GetStudentListHandler).Methods("GET")
+// 	mux.HandleFunc("/students/{id:[0-9]+}", GetStudentHandler).Methods("GET")
+// 	mux.HandleFunc("/students", PostStudentHandler).Methods("GET")
+// 	mux.HandleFunc("/students/{id:[0-9]+}", DeleteStudentHandler).Methods("DELETE")
+
+// 	students = make(map[int]Student)
+// 	students[1] = Student{1, "aaa", 16, 87}
+// 	students[2] = Student{2, "bbb", 18, 98}
+// 	lastId = 2
+
+// 	return mux
+// }
+
+func SetupHandlers(g *gin.Engine) {
+	// 웹핸들러를 셋팅
+	g.GET("/students", GetStudentsHandler)
+	g.GET("/student/:id", GetStudentHandler)
+	g.POST("/student", PostStudentHandler)
+	g.DELETE("/student/:id", DeleteStudentHandler)
 
 	students = make(map[int]Student)
 	students[1] = Student{1, "aaa", 16, 87}
 	students[2] = Student{2, "bbb", 18, 98}
 	lastId = 2
-
-	return mux
 }
 
-type Students []Student
+type Students []Student // Id로 정렬하는 인터페이스
 
 func (s Students) Len() int {
 	return len(s)
@@ -46,58 +66,123 @@ func (s Students) Less(i, j int) bool {
 	return s[i].Id < s[j].Id
 }
 
-func GetStudentListHandler(w http.ResponseWriter, r *http.Request) {
+// func GetStudentListHandler(w http.ResponseWriter, r *http.Request) {
+// 	list := make(Students, 0)
+// 	for _, student := range students {
+// 		list = append(list, student)
+// 		sort.Sort(list)
+// 		w.WriteHeader(http.StatusOK)
+// 		w.Header().Set("Content-Type", "application/json")
+// 		json.NewEncoder(w).Encode(list)
+// 	}
+// }
+
+func GetStudentsHandler(c *gin.Context) {
 	list := make(Students, 0)
 	for _, student := range students {
 		list = append(list, student)
+
 		sort.Sort(list)
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(list)
+		c.JSON(http.StatusOK, list) // JSON 포맷으로 변환
 	}
 }
 
-func GetStudentHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r) // id를 가져옵니다.
-	id, _ := strconv.Atoi(vars["id"])
-	student, ok := students[id]
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		// id에 해당하는 학생이 없으면 에러
+// func GetStudentHandler(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r) // id를 가져옵니다.
+// 	id, _ := strconv.Atoi(vars["id"])
+// 	student, ok := students[id]
+// 	if !ok {
+// 		w.WriteHeader(http.StatusNotFound)
+// 		// id에 해당하는 학생이 없으면 에러
+// 		return
+// 	}
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Header().Set("Content-Type", "application/json")
+// 	json.NewEncoder(w).Encode(student)
+// }
+
+func GetStudentHandler(c *gin.Context) {
+	idstr := c.Param("id")
+	if idstr == "" {
+		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(student)
+
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
+	student, ok := students[id]
+	if !ok {
+		c.AbortWithStatus(http.StatusNotFound) // 에러 반환
+		return
+	}
+	c.JSON(http.StatusOK, student)
 }
 
-func PostStudentHandler(w http.ResponseWriter, r *http.Request) {
+// func PostStudentHandler(w http.ResponseWriter, r *http.Request) {
+// 	var student Student
+// 	err := json.NewDecoder(r.Body).Decode(&student) // JSON 데이터 반환
+// 	if err != nil {
+// 		w.WriteHeader(http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	lastId++
+// 	student.Id = lastId // id를 증가시킨 후 앱에 등록
+// 	students[lastId] = student
+// 	w.WriteHeader(http.StatusCreated)
+// }
+
+func PostStudentHandler(c *gin.Context) {
 	var student Student
-	err := json.NewDecoder(r.Body).Decode(&student) // JSON 데이터 반환
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+
+	if err := c.ShouldBindJSON(&student); err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
 		return
 	}
 
 	lastId++
-	student.Id = lastId // id를 증가시킨 후 앱에 등록
+	student.Id = lastId
 	students[lastId] = student
-	w.WriteHeader(http.StatusCreated)
+	c.String(http.StatusCreated, "Success to add id: %d", lastId)
 }
 
-func DeleteStudentHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r) // id를 가져옵니다.
-	id, _ := strconv.Atoi(vars["id"])
-	_, ok := students[id]
-	if !ok {
-		w.WriteHeader(http.StatusNotFound)
-		// id에 해당하는 학생이 없으면 에러
+// func DeleteStudentHandler(w http.ResponseWriter, r *http.Request) {
+// 	vars := mux.Vars(r) // id를 가져옵니다.
+// 	id, _ := strconv.Atoi(vars["id"])
+// 	_, ok := students[id]
+// 	if !ok {
+// 		w.WriteHeader(http.StatusNotFound)
+// 		// id에 해당하는 학생이 없으면 에러
+// 		return
+// 	}
+// 	delete(students, id)
+// 	w.WriteHeader(http.StatusOK) // statusOK 반환
+// }
+
+func DeleteStudentHandler(c *gin.Context) {
+	idstr := c.Param("id")
+	if idstr == "" {
+		c.AbortWithStatus(http.StatusBadRequest)
 		return
 	}
+
+	id, err := strconv.Atoi(idstr)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+
 	delete(students, id)
-	w.WriteHeader(http.StatusOK) // statusOK 반환
+	c.String(http.StatusOK, "success to delete")
 }
 
 func main() {
-	http.ListenAndServe(":3000", MakeWebHandler())
+	// http.ListenAndServe(":3000", MakeWebHandler())
+	r := gin.Default()
+	SetupHandlers(r)
+	r.Run(":3000")
 }
--- main_test.go
+++ main_test.go
@@ -1,102 +1,102 @@
 package main
 
-import (
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
-	"strings"
-	"testing"
+// import (
+// 	"encoding/json"
+// 	"net/http"
+// 	"net/http/httptest"
+// 	"strings"
+// 	"testing"
 
-	"github.com/stretchr/testify/assert"
-)
+// 	"github.com/stretchr/testify/assert"
+// )
 
-func TestJsonHandler(t *testing.T) {
-	assert := assert.New(t)
+// func TestJsonHandler(t *testing.T) {
+// 	assert := assert.New(t)
 
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/students", nil)
+// 	res := httptest.NewRecorder()
+// 	req := httptest.NewRequest("GET", "/students", nil)
 
-	// /students 경로 테스트
-	mux := MakeWebHandler()
-	mux.ServeHTTP(res, req)
+// 	// /students 경로 테스트
+// 	mux := MakeWebHandler()
+// 	mux.ServeHTTP(res, req)
 
-	assert.Equal(http.StatusOK, res.Code)
-	var list []Student
-	err := json.NewDecoder(res.Body).Decode(&list) // 결과 반환
+// 	assert.Equal(http.StatusOK, res.Code)
+// 	var list []Student
+// 	err := json.NewDecoder(res.Body).Decode(&list) // 결과 반환
 
-	// 결과 확인
-	assert.Nil(err)
-	assert.Equal(2, len(list))
-	assert.Equal("aaa", list[0].Name)
-	assert.Equal("bbb", list[1].Name)
-}
+// 	// 결과 확인
+// 	assert.Nil(err)
+// 	assert.Equal(2, len(list))
+// 	assert.Equal("aaa", list[0].Name)
+// 	assert.Equal("bbb", list[1].Name)
+// }
 
-func TestJsonHandler2(t *testing.T) {
-	assert := assert.New(t)
+// func TestJsonHandler2(t *testing.T) {
+// 	assert := assert.New(t)
 
-	var student Student
+// 	var student Student
 
-	mux := MakeWebHandler()
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("GET", "/students/1", nil) // id 1 학생
+// 	mux := MakeWebHandler()
+// 	res := httptest.NewRecorder()
+// 	req := httptest.NewRequest("GET", "/students/1", nil) // id 1 학생
 
-	mux.ServeHTTP(res, req)
-	assert.Equal(http.StatusOK, res.Code)
-	err := json.NewDecoder(res.Body).Decode(&student)
-	assert.Nil(err)
-	assert.Equal("aaa", student.Name)
+// 	mux.ServeHTTP(res, req)
+// 	assert.Equal(http.StatusOK, res.Code)
+// 	err := json.NewDecoder(res.Body).Decode(&student)
+// 	assert.Nil(err)
+// 	assert.Equal("aaa", student.Name)
 
-	res = httptest.NewRecorder()
-	req = httptest.NewRequest("GET", "students/2", nil) // id 2 학생
-	mux.ServeHTTP(res, req)
+// 	res = httptest.NewRecorder()
+// 	req = httptest.NewRequest("GET", "students/2", nil) // id 2 학생
+// 	mux.ServeHTTP(res, req)
 
-	assert.Equal(http.StatusOK, res.Code)
-	err = json.NewDecoder(res.Body).Decode(&student)
-	assert.Nil(err)
-	assert.Equal("bbb", student.Name)
-}
+// 	assert.Equal(http.StatusOK, res.Code)
+// 	err = json.NewDecoder(res.Body).Decode(&student)
+// 	assert.Nil(err)
+// 	assert.Equal("bbb", student.Name)
+// }
 
-func TestJsonHandler3(t *testing.T) {
-	assert := assert.New(t)
+// func TestJsonHandler3(t *testing.T) {
+// 	assert := assert.New(t)
 
-	var student Student
+// 	var student Student
 
-	mux := MakeWebHandler()
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("POST", "/students",
-		strings.NewReader(`{"Id" :0, "Name": "ccc", "Age": 15, "Score": 78}`)) // 새로운 학생 데이터
+// 	mux := MakeWebHandler()
+// 	res := httptest.NewRecorder()
+// 	req := httptest.NewRequest("POST", "/students",
+// 		strings.NewReader(`{"Id" :0, "Name": "ccc", "Age": 15, "Score": 78}`)) // 새로운 학생 데이터
 
-	mux.ServeHTTP(res, req)
-	assert.Equal(http.StatusOK, res.Code) // 응답 코드 검사
+// 	mux.ServeHTTP(res, req)
+// 	assert.Equal(http.StatusOK, res.Code) // 응답 코드 검사
 
-	res = httptest.NewRecorder()
-	req = httptest.NewRequest("GET", "students/3", nil) // 추가된 학생 데이터
-	mux.ServeHTTP(res, req)
+// 	res = httptest.NewRecorder()
+// 	req = httptest.NewRequest("GET", "students/3", nil) // 추가된 학생 데이터
+// 	mux.ServeHTTP(res, req)
 
-	assert.Equal(http.StatusOK, res.Code)
-	err := json.NewDecoder(res.Body).Decode(&student)
-	assert.Nil(err)
-	assert.Equal("ccc", student.Name)
-}
+// 	assert.Equal(http.StatusOK, res.Code)
+// 	err := json.NewDecoder(res.Body).Decode(&student)
+// 	assert.Nil(err)
+// 	assert.Equal("ccc", student.Name)
+// }
 
-func TestJsonHandler4(t *testing.T) {
-	assert := assert.New(t)
+// func TestJsonHandler4(t *testing.T) {
+// 	assert := assert.New(t)
 
-	mux := MakeWebHandler()
-	res := httptest.NewRecorder()
-	req := httptest.NewRequest("DELETE", "/students/1", nil) // DELETE 요청
+// 	mux := MakeWebHandler()
+// 	res := httptest.NewRecorder()
+// 	req := httptest.NewRequest("DELETE", "/students/1", nil) // DELETE 요청
 
-	mux.ServeHTTP(res, req)
-	assert.Equal(http.StatusOK, res.Code) // 응답 코드 검사
+// 	mux.ServeHTTP(res, req)
+// 	assert.Equal(http.StatusOK, res.Code) // 응답 코드 검사
 
-	res = httptest.NewRecorder()
-	req = httptest.NewRequest("GET", "students", nil) // students 경로
-	mux.ServeHTTP(res, req)
+// 	res = httptest.NewRecorder()
+// 	req = httptest.NewRequest("GET", "students", nil) // students 경로
+// 	mux.ServeHTTP(res, req)
 
-	assert.Equal(http.StatusOK, res.Code)
-	var list []Student
-	err := json.NewDecoder(res.Body).Decode(&list)
-	assert.Nil(err)
-	assert.Equal(1, len(list)) // 결과 확인
-	assert.Equal("ccc", list[0].Name)
-}
+// 	assert.Equal(http.StatusOK, res.Code)
+// 	var list []Student
+// 	err := json.NewDecoder(res.Body).Decode(&list)
+// 	assert.Nil(err)
+// 	assert.Equal(1, len(list)) // 결과 확인
+// 	assert.Equal("ccc", list[0].Name)
+// }
